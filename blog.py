1bea2baccc49e0f84d66d6d25852336eebd0d07c
Lukmannudin <laodelukmannudinpriatna@gmail.com>
2020-08-10 02:02:26 +0700

    update

--- blog.py
+++ blog.py
@@ -2,6 +2,9 @@ import os
 
 import numpy as np
 from flask import Flask, render_template, request, send_from_directory
+from keras.layers import Conv2D, Dense, Flatten, Dropout
+from keras.models import Sequential
+from keras.optimizers import SGD
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from tensorflow import keras
@@ -92,3 +95,73 @@ def upload():
         result = "Masih gak yakin"
 
     return render_template("index.html", result=result)
+
+
+@app.route('/training-data')
+def traindata():
+    ROOT_PATH = "E:/projects/comvistugas2/tmp/dataset"
+    CLASS_MODE = 'categorical'
+    COLOR_MODE = 'grayscale'
+
+    BATCH_SIZE = 15
+    TARGET_SIZE = (48, 48)
+
+    EPOCH = 30
+
+    dataset_generator = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.3,
+        rotation_range=0.3,
+        validation_split=0.2,
+        zoom_range=0.2
+    )
+
+    validation_generator = ImageDataGenerator(
+        rescale=1. / 255,
+        validation_split=0.2
+    )
+
+    train_set = dataset_generator.flow_from_directory(
+        ROOT_PATH,
+        target_size=TARGET_SIZE,
+        batch_size=BATCH_SIZE,
+        color_mode=COLOR_MODE,
+        class_mode=CLASS_MODE,
+        subset='training'
+    )
+
+    validation_set = validation_generator.flow_from_directory(
+        ROOT_PATH,
+        target_size=TARGET_SIZE,
+        batch_size=BATCH_SIZE,
+        color_mode=COLOR_MODE,
+        class_mode=CLASS_MODE,
+        subset='validation',
+        shuffle=False
+    )
+
+    train_set.class_indices
+
+    model = Sequential([
+        Conv2D(64, (3, 3), input_shape=(TARGET_SIZE[0], TARGET_SIZE[1], 1), activation='relu'),
+        Dropout(0.2),
+        Flatten(),
+        Dense(32, activation='relu'),
+        Dropout(0.2),
+        Dense(16, activation='relu'),
+        Dense(2, activation='softmax')
+    ])
+
+    optimizer = SGD(learning_rate=0.1)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    model.fit(
+        train_set,
+        epochs=EPOCH,
+        validation_data=validation_set
+    )
+
+    model.save("modelcomvisbaru.h5")
+
+
